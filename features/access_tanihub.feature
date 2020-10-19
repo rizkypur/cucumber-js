1ee0e1daf804c76025c70a17b74f1c1aaae672b3
rizkypur <rizubuntu@gmail.com>
2020-10-19 09:39:23 +0700

    Initial Commit

---
+++ features/access_tanihub.feature
@@ -0,0 +1,6 @@
+Feature: Access TaniHub
+
+        Scenario: Click Area Pengiriman Jabodetabek
+            Given I am accesing tanihub website
+             When I click Jabodetabek for Area Pengiriman
+             Then the page title should be Majukan Perekonomian Lokal | TaniHub
---
+++ features/step_definitions/access_tanihub.js
@@ -0,0 +1,40 @@
+const { Given, When, Then, AfterAll, BeforeAll, Before } = require('@cucumber/cucumber');
+const { Builder, By, Capabilities, Key, until } = require('selenium-webdriver');
+const { expect } = require('chai');
+
+var {setDefaultTimeout} = require('@cucumber/cucumber');
+const { build } = require('protobufjs');
+const { after } = require('lodash');
+setDefaultTimeout(60 * 1000);
+
+
+require("chromedriver");
+
+// driver setup
+const capabilities = Capabilities.chrome();
+capabilities.set('chromeOptions', { "w3c": false });
+const driver = new Builder().withCapabilities(capabilities).build();
+
+BeforeAll(async function () {
+  await driver.manage().window().maximize();
+}); 
+
+Given('I am accesing tanihub website', async function () {
+    await driver.get('http://tanihub.com');
+});
+
+When('I click Jabodetabek for Area Pengiriman', async function () {
+    const element = await driver.findElement(By.xpath("//div[@class='regionModalBody-0-2-22']/div[2]"));
+    element.click();
+});
+
+Then('the page title should be Majukan Perekonomian Lokal | TaniHub', async function () {
+     const title = await driver.getTitle();
+    // const isTitleStartWithCheese = title.toLowerCase().lastIndexOf(`${searchTerm}`, 0) === 0;
+     expect(title).to.equal('Majukan Perekonomian Lokal | TaniHub');
+});
+
+
+  AfterAll(async function () {
+    await driver.quit();
+  });  
